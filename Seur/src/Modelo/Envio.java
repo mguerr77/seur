f43f921e4f9cb726d8d8c1d948e233da4fc3dd70
mguerr77 <109104193+mguerr77@users.noreply.github.com>
2022-12-13 20:36:16 +0100

    calse envío completa

--- Seur/src/Modelo/Envio.java
+++ Seur/src/Modelo/Envio.java
@@ -24,9 +24,87 @@ public class Envio {
 		this.tipoEnvio = tipoEnvio;
 		this.peso = peso;
 	}
+
+
+	public double calcularImporte()
+	{
+		double coste=0;
+		if (this.destinoNacional && this.origenNacional)
+		{
+			coste=coste + 4; 
+		}
+		else
+			coste=coste+7;
+		if (this.tipoEnvio.equals("Paq 10 - Antes de las 10 h ") )
+		{
+			coste=coste + 5;
+		}
+		coste=coste + this.peso * 0.1;
+		return coste; 
+	}
+	
+	
+	public String getCiudadOrigen() {
+		return ciudadOrigen;
+	}
+
+
+	public void setCiudadOrigen(String ciudadOrigen) {
+		this.ciudadOrigen = ciudadOrigen;
+	}
+
+
+	public boolean isOrigenNacional() {
+		return origenNacional;
+	}
+
+
+	public void setOrigenNacional(boolean origenNacional) {
+		this.origenNacional = origenNacional;
+	}
+
+
+	public String getCiudadDestino() {
+		return ciudadDestino;
+	}
+
+
+	public void setCiudadDestino(String ciudadDestino) {
+		this.ciudadDestino = ciudadDestino;
+	}
+
+
+	public boolean isDestinoNacional() {
+		return destinoNacional;
+	}
+
+
+	public void setDestinoNacional(boolean destinoNacional) {
+		this.destinoNacional = destinoNacional;
+	}
+
+
+	public String getTipoEnvio() {
+		return tipoEnvio;
+	}
+
+
+	public void setTipoEnvio(String tipoEnvio) {
+		this.tipoEnvio = tipoEnvio;
+	}
+
+
+	public int getPeso() {
+		return peso;
+	}
+
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
 	
 		
-	}
+	
 	
 
 }
